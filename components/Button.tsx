38ac7e4f1f031ebc4ffa817e054ece7abd5fc6a1
jmillstein <josh@mvretail.com>
2024-10-09 11:26:30 -0500

    slice variations

---
+++ components/Button.tsx
@@ -0,0 +1,13 @@
+import clsx from "clsx";
+import { PrismicNextLink, PrismicNextLinkProps } from "@prismicio/next";
+
+export default function Button({
+    className,
+    ...restProps
+}: PrismicNextLinkProps) {
+    return(
+        <PrismicNextLink 
+            className={clsx("block w-fit bg-cyan-700 hover:bg-cyan-800 transition-colors duration-300 text-white font-bold py-3 px-12 rounded-full text-white font-nunito-sans tracking-wide text-base mb-8 md:mb-10", className)} {...restProps}
+        />
+    );
+}
---
+++ components/Heading.tsx
@@ -0,0 +1,28 @@
+import clsx from "clsx"
+
+type HeadingProps = {
+    as?: "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+    size?: "sm" | "md" | "lg" | "xl";
+    children: React.ReactNode
+    className?: string;
+}
+
+export default function Heading({
+    as: Comp = "h1",
+    className,
+    children,
+    size = "lg"
+}: HeadingProps) {
+    return (
+        <Comp className={clsx("leading-tight text-slate-700 font-bold font-nunito",
+            size === "xl" && "text-5xl md:text-7xl",
+            size === "lg" && "text-4xl md:text-5xl",
+            size === "md" && "text-3xl md:text-4xl",
+            size === "sm" && "text-2xl md:text-3xl", 
+            className
+        )}
+    >
+        {children}  
+    </Comp>
+    )
+}
