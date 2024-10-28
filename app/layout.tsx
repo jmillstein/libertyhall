986c42172cd95ff0f5bac7f10c7b7aabc8cdecef
jmillstein <josh@mvretail.com>
2024-10-28 14:22:51 -0500

    preview work

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,9 +3,10 @@ import localFont from "next/font/local";
 import "./globals.css";
 import clsx from "clsx";
 import {Nunito, Nunito_Sans} from "next/font/google";
-import { createClient } from "@/prismicio";
+import { createClient, repositoryName } from "@/prismicio";
 import Header from "@/components/Header";
 import Footer from "@/components/Footer";
+import { PrismicPreview } from "@prismicio/next";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -54,6 +55,7 @@ export default function RootLayout({
         {children}
         <Footer />
         <div className="fixed bg-gradient-to-tr from-emerald-50 to-cyan-50 z-[-1] inset-0 opacity-50" />
+        <PrismicPreview repositoryName={repositoryName} />
       </body>
     </html>
   );
